2af2cc33a37f516636802db2b8d145167759b63b
Anthony Azubike Osigbe <thronesofglory@gmail.com>
2023-03-13 15:40:09 +0100

    Update task2.php

--- task2.php
+++ task2.php
@@ -1,13 +1,17 @@
 <?php
-    $pvc = 'true';
-    $age = 17;
+    $age = 18;
     $ward = 020;
+    $pvc = 'true';
+    
 
     if ($age < 18){
+
       echo "Voter not up to voting age <br> Not Eligible to vote";
     }elseif($pvc == 'false'){
+
       echo "No voters card: Not eligible to vote";
        }else{
+
       echo "Voter eligible to vote";
     }
 
