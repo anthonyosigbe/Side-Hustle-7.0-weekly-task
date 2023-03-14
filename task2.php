1790c081e890866239d2db47941a2050ad82e73c
Anthony Azubike Osigbe <thronesofglory@gmail.com>
2023-03-14 11:28:38 +0100

    Update task2.php

--- task2.php
+++ task2.php
@@ -1,18 +1,21 @@
 <?php
     $age = 18;
-    $ward = 020;
+    $ward = 20;
     $pvc = 'true';
     
 
     if ($age < 18){
 
-      echo "Voter not up to voting age <br> Not Eligible to vote";
+      echo "Voter not up to voting age  <br> <h1>Voter not Eligible to vote</h1>";
     }elseif($pvc == 'false'){
 
-      echo "No voters card: Not eligible to vote";
-       }else{
+      echo "No voters card <br><h1> Voter not eligible to vote</h1>";
+       }elseif ($ward <>20) {
+        echo "Incorrect Voters ward <br> <h1>Voter not eligible to vote in this ward</h1>";
+       }
+       else{
 
-      echo "Voter eligible to vote";
+      echo "Voter details correct <br><h1>Voter eligible to vote</h1>";
     }
 
     
